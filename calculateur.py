84751c330851cc1f3afecbde7f15b15e1d71cfac
WalaeBENAMARA <benamarawalae@icloud.com>
2026-05-29 12:59:50 +0200

    Ajout fonction calculer_remise

--- calculateur.py
+++ calculateur.py
@@ -4,3 +4,11 @@ def calculer_tva(prix_ht: float) -> float:
 
 prix_ttc = calculer_tva(100)
 print(f"Total TTC: {prix_ttc}")
+
+def calculer_remise(prix: float, remise_pct: float) -> float:
+    """
+    Applique une remise en pourcentage sur un prix.
+
+    Exemple : calculer_remise(100, 20) -> 80.0
+    """
+    return prix * (1 - remise_pct / 100)
